9b3b69f7beff9d0e37cb7d99874cf957640b3045
AhmedEsaam005 <Ahmed.Amer@eng-st.cu.edu.eg>
2026-04-10 16:33:08 +0200

    refactor: reorganize code structure and enhance user authentication endpoints

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
 const app = express();
 app.use(express.json());
+const mongoose = require("mongoose");
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+
 const users = require("./models/user");
 const tasks = require("./models/task");
 const modes = require("./models/modes");
@@ -17,29 +19,95 @@ mongoose
 	.catch((error) => {
 		console.log("Error Connceting Database", error);
 	});
-app.get("/getAllUsers", async (req, res) => {
+/////////////////////////////////////////////////////////////
+
+app.get("/login", async (req, res) => {
 	try {
-		const allUsers = await users.find();
-		res.json(allUsers);
-	} catch (err) {
-		res.status(500).json({ error: err.message });
+		const { userEmail, userPassword } = req.body;
+		const user = await users.findOne({ email: userEmail });
+		if (!user) {
+			return res.json({ message: "Email Does Not Exist" });
+		}
+		const isMatch = await bcrypt.compare(userPassword, user.password);
+
+		if (!isMatch) {
+			return res.json({ message: "Password Isn`t Correct" });
+		}
+		res.json({
+			name: user.name,
+			age: user.age,
+			profileImage: user.profileImage,
+			theme: user.theme,
+		});
+	} catch (error) {
+		res.status(500).json({ error: error.message });
 	}
 });
 
-app.get("/getAllModes", async (req, res) => {
+app.post("/signUp", async (req, res) => {
 	try {
-		const allModes = await modes.find();
-		res.json(allModes);
-	} catch (err) {
-		res.status(500).json({ error: err.message });
+		const { userEmail, userPassword, userName, userAge } = req.body;
+		const alreadyExist = await users.findOne({ email: userEmail });
+		if (alreadyExist) {
+			return res.json({ message: "Email Already Exists" });
+		}
+		const hashedPassword = await bcrypt.hash(userPassword, 10);
+		const user = await users.create({
+			email: userEmail,
+			password: hashedPassword,
+			name: userName,
+			age: userAge,
+		});
+		res.json({ message: "sucess" });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
 	}
 });
 
+app.get("/getUserModes", async (req, res) => {
+	try {
+		const userEmail = req.body.email;
+		const userPlatform = req.body.platform;
+		const userID = await users.findOne({ email: userEmail }).select("_id");
+		const allModes = await modes.find({
+			userId: userID,
+			platform: userPlatform,
+		});
+		if (!userID) {
+			res.json({ message: "This User Does Not Exists" });
+		} else res.json(allModes);
+	} catch (err) {
+		res.status(500).json({ error: error.message });
+	}
+});
+
+app.get("/getUserTasks", async (req, res) => {
+	try {
+		const userEmail = req.body.email;
+		const userID = await users.findOne({ email: userEmail }).select("_id");
+		const allTasks = await tasks.find({ userId: userID });
+		if (!userID) {
+			res.json({ message: "This User Does Not Exists" });
+		} else res.send(allTasks);
+	} catch (err) {
+		res.status(500).json({ error: error.message });
+	}
+});
+
+/////////////////////////////////////////////////////////////
 app.get("/getAllTasks", async (req, res) => {
 	try {
 		const allTasks = await tasks.find();
 		res.json(allTasks);
 	} catch (err) {
-		res.status(500).json({ error: err.message });
+		res.status(500).json({ error: error.message });
+	}
+});
+app.get("/getAllUsers", async (req, res) => {
+	try {
+		const allUsers = await users.find();
+		res.json(allUsers);
+	} catch (err) {
+		res.status(500).json({ error: error.message });
 	}
 });
